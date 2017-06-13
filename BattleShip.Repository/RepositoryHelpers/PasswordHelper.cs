832313817a5027827a9b527535b4db779200ea64
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-14 01:34:57 +0200

    Add PasswordHelper class and a GetSha512CngPasswordHash method

---
+++ BattleShip.Repository/RepositoryHelpers/PasswordHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BattleShip.Repository.RepositoryHelpers
+{
+    public static class PasswordHelper
+    {
+        public static string GetSha512CngPasswordHash(string password)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(password);
+            byte[] hash;
+
+            using (SHA512Cng sha512Cng = new SHA512Cng())
+            {
+                hash = sha512Cng.ComputeHash(data);
+            }
+
+            return Encoding.UTF8.GetString(hash);
+        }
+    }
+}
