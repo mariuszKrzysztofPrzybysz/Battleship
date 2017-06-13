605323e80ca54f8b1b81c0b7a0a8580cd3d4f599
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-14 00:24:30 +0200

    Add BattleShipContext class

---
+++ BattleShip.Database/BattleShipContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace BattleShip.Database
+{
+    public class BattleShipContext :DbContext
+    {
+        public BattleShipContext() : base("name=BattleShipConnection")
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
