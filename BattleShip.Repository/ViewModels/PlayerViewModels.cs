3dd64933a3f4c8c6738bc6f65b0125b38f0d9494
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-14 01:46:20 +0200

    Add AddPlayerViewModel view model

--- BattleShip.Repository/ViewModels/PlayerViewModels.cs
+++ BattleShip.Repository/ViewModels/PlayerViewModels.cs
@@ -1,4 +1,41 @@
-﻿namespace BattleShip.Repository.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using BattleShip.Database.Entities;
+
+namespace BattleShip.Repository.ViewModels
 {
-    
+    public class AddPlayerViewModel
+    {
+        [Required]
+        [Index(IsUnique = true)]
+        [MaxLength(50)]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email address")]
+        public string EmailAddress { get; set; }
+
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        [Required]
+        [Display(Name = "Allow new battle")]
+        public bool AllowNewBattle { get; set; }
+
+        [Required]
+        [Display(Name = "Allow private chat")]
+        public bool AllowPrivateChat { get; set; }
+    }
 }
