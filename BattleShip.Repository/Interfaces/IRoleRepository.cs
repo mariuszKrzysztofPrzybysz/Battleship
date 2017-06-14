4cf7abcaa0ade1ca34389f964cd53a5d1b458ea5
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-14 04:02:59 +0200

    Implement GetAllRoles and GetRoleIdOrDefault methods

---
+++ BattleShip.Repository/Interfaces/IRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BattleShip.Database.Entities;
+using BattleShip.Repository.RepositoryHelpers;
+
+namespace BattleShip.Repository.Interfaces
+{
+    public interface IRoleRepository
+    {
+        IEnumerable<Role> GetAllRoles();
+        byte GetRoleIdOrDefault(string name);
+    }
+}
