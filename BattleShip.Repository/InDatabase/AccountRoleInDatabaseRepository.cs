7dcbdba39288ad3374fecb28bee878fb43c2849f
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-14 12:16:32 +0200

    Implement GetAccountRoles method

--- BattleShip.Repository/InDatabase/AccountRoleInDatabaseRepository.cs
+++ BattleShip.Repository/InDatabase/AccountRoleInDatabaseRepository.cs
@@ -1,9 +1,28 @@
-﻿using BattleShip.Repository.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BattleShip.Database;
+using BattleShip.Database.Entities;
+using BattleShip.Repository.Interfaces;
 
 namespace BattleShip.Repository.InDatabase
 {
     public class AccountRoleInDatabaseRepository : IAccountRoleRepository
     {
-        
+        private readonly BattleShipContext _context;
+
+        public AccountRoleInDatabaseRepository(BattleShipContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Role> GetAccountRoles(string login)
+        {
+            return _context.Roles
+                .Where(r =>
+                    r.AccountRoles.Any(ar =>
+                        ar.Account.Login.Equals(login, StringComparison.OrdinalIgnoreCase)))
+                .ToList();
+        }
     }
 }
--- BattleShip.Repository/Interfaces/IAccountRoleRepository.cs
+++ BattleShip.Repository/Interfaces/IAccountRoleRepository.cs
@@ -1,7 +1,11 @@
-﻿namespace BattleShip.Repository.Interfaces
+﻿using System.Collections;
+using System.Collections.Generic;
+using BattleShip.Database.Entities;
+
+namespace BattleShip.Repository.Interfaces
 {
     public interface IAccountRoleRepository
     {
-
+        IEnumerable<Role> GetAccountRoles(string login);
     }
 }
