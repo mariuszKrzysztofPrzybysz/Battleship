c52e1851c8149134398efdf8bd729d2e73632a2e
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-14 14:39:02 +0200

    Add Chat controller and Index view

---
+++ BattleShip.Web/Controllers/ChatController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BattleShip.Repository.Interfaces;
+
+namespace BattleShip.Web.Controllers
+{
+    public class ChatController : Controller
+    {
+        private readonly IAccountRepository _repository;
+
+        public ChatController(IAccountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
