6d49733b39334bcefa65cc5e5291b71e93bd291d
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-14 02:07:21 +0200

    Implement IoC container

--- BattleShip.Web/Global.asax.cs
+++ BattleShip.Web/Global.asax.cs
@@ -1,21 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using BattleShip.Web.Infrastructure;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
 
 namespace BattleShip.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static IWindsorContainer _container;
+
+        private static void BoostratpContainer()
+        {
+            _container = new WindsorContainer().Install(FromAssembly.This());
+
+            var controllerFactory = new WindsorControllerFactory(_container.Kernel);
+
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            BoostratpContainer();
+        }
+
+        protected void Application_End()
+        {
+            _container.Dispose();
         }
     }
 }
---
+++ BattleShip.Web/Infrastructure/ServicesInstaller.cs
@@ -0,0 +1,24 @@
+﻿using BattleShip.Database;
+using BattleShip.Repository.InDatabase;
+using BattleShip.Repository.Interfaces;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace BattleShip.Web.Infrastructure
+{
+    public class ServicesInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container
+                .Register(Component
+                    .For<BattleShipContext>());
+
+            container
+                .Register(Component
+                    .For<IPlayerRepository>()
+                    .ImplementedBy<PlayerInDatabaseRepository>());
+        }
+    }
+}
