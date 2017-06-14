42ebce386e194137de1d6db4bf7612b4684e72f4
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-14 02:15:48 +0200

    Add RegisterAccountViewModel view model

---
+++ BattleShip.Web/ViewModels/RegisterAccountViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BattleShip.Repository.ViewModels;
+
+namespace BattleShip.Web.ViewModels
+{
+    public class RegisterAccountViewModel : AddPlayerViewModel
+    {
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
