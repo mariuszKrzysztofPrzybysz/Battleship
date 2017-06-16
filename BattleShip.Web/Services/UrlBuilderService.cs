12bb0106d8d6e77b15a22705cfcc9b2dd8a4a05f
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-16 02:30:32 +0200

    Add UrlBuilderService class for generating url addresses

---
+++ BattleShip.Web/Services/UrlBuilderService.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+
+namespace BattleShip.Web.Services
+{
+    public static class UrlBuilderService
+    {
+        private static readonly string Scheme = HttpContext.Current.Request.Url.Scheme;
+        private static readonly string Authority = HttpContext.Current.Request.Url.Authority;
+
+        public static string GetUrl(string controller, string action)
+        {
+            return $"{Scheme}://{Authority}/{controller}/{action}";
+        }
+
+        public static string GetUrl(string controller, string action, string[] queryStrings)
+        {
+            var result = GetUrl(controller, action);
+
+            if (queryStrings != null)
+            {
+                if (queryStrings.Length > 0)
+                    result += $"?{queryStrings[0]}";
+                for (byte i = 1; i < queryStrings.Length; i++)
+                    result += $"&{queryStrings[i]}";
+            }
+
+            return result;
+        }
+    }
+}
