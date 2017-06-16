da93826906cef165ba9e02cae090be0b150302d5
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-16 23:36:09 +0200

    BugFix

--- BattleShip.Web/Attributes/BattleShipAuthorizeAttribute.cs
+++ BattleShip.Web/Attributes/BattleShipAuthorizeAttribute.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using BattleShip.Database.Entities;
+using BattleShip.Web.Services;
 
 namespace BattleShip.Web.Attributes
 {
@@ -20,20 +21,26 @@ namespace BattleShip.Web.Attributes
             base.OnAuthorization(filterContext);
             if (!string.IsNullOrEmpty(_role))
             {
-                var rolesList = filterContext.HttpContext.Session["Roles"] as IEnumerable<Role>;
-
-                if (!rolesList.Any(x => x.Name.Equals(_role, StringComparison.OrdinalIgnoreCase)))
+                if (filterContext.HttpContext.Session != null)
                 {
-                    var url = new UrlHelper(filterContext.RequestContext);
-                    var errorUrl = url.Action("Index", "Errors",
-                        new
+                    var rolesList = filterContext.HttpContext.Session["Roles"] as IEnumerable<Role>;
 
-                        {
-                            message = $"You are not authorized to make changes " +
-                                      $"kontroler {filterContext.ActionDescriptor.ControllerDescriptor.ControllerName}" +
-                                      $", akcja {filterContext.ActionDescriptor.ActionName}"
-                        });
-                    filterContext.Result = new RedirectResult(errorUrl);
+                    if (rolesList == null)
+                        filterContext.Result = new RedirectResult(UrlBuilderService.GetUrl("Home","Index"));
+
+                    else if (!rolesList.Any(x => x.Name.Equals(_role, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        var url = new UrlHelper(filterContext.RequestContext);
+                        var errorUrl = url.Action("Index", "Error",
+                            new
+
+                            {
+                                message = $"You are not authorized to make changes " +
+                                          $"kontroler {filterContext.ActionDescriptor.ControllerDescriptor.ControllerName}" +
+                                          $", akcja {filterContext.ActionDescriptor.ActionName}"
+                            });
+                        filterContext.Result = new RedirectResult(errorUrl);
+                    }
                 }
             }
         }
