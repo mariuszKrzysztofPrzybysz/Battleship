9ecf013525bd8c8b00975431d4effaed561e6b7a
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-17 23:22:23 +0200

    Add CreateBattleViewModel class

---
+++ BattleShip.Repository/ViewModels/BattleViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BattleShip.Repository.ViewModels
+{
+    public class CreateBattleViewModel
+    {
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Player name")]
+        public string PlayerName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Opponent name")]
+        public string OpponentName { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime StartUtcDateTime { get; set; }
+    }
+}
