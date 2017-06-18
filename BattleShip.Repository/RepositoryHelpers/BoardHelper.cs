c06df5a48154cc3766cfb783ad9ad09dcb7b81ff
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-19 01:29:24 +0200

    Add BoardHelper class with IsValid method declaration

---
+++ BattleShip.Repository/RepositoryHelpers/BoardHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BattleShip.Repository.RepositoryHelpers
+{
+    public static class BoardHelper
+    {
+        public static bool IsValid(string board)
+        {
+            //TODO: Validate board
+            return true;
+        }
+    }
+}
