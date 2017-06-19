d35c8f37a8ed8cda2262ab181463ffcdec725f8b
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-20 01:22:09 +0200

    Add GetUrl method to generate url addresses

--- BattleShip.Web/Helpers/UrlBuilder.cs
+++ BattleShip.Web/Helpers/UrlBuilder.cs
@@ -1,6 +1,31 @@
-﻿namespace BattleShip.Web.Helpers
+﻿using System.Web;
+
+namespace BattleShip.Web.Helpers
 {
-    public class UrlBuilder
+    public static class UrlBuilder
     {
+        private static readonly string Scheme = HttpContext.Current.Request.Url.Scheme;
+        private static readonly string Authority = HttpContext.Current.Request.Url.Authority;
+
+        private static string GetUrl(string controller, string action)
+        {
+            return $"{Scheme}://{Authority}/{controller}/{action}";
+        }
+
+        public static string GetUrl(string controller, string action, string[] queries)
+        {
+            var result = GetUrl(controller, action);
+
+            if (queries == null)
+                return result;
+
+            if (queries.Length > 0)
+                result += $"?{queries[0]}";
+
+            for (byte i = 1; i < queries.Length; i++)
+                result += $"&{queries[i]}";
+
+            return result;
+        }
     }
 }
