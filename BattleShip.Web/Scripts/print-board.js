d24f8d261bc253f06efba14970d738cf3c811c99
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-22 19:51:40 +0200

    Add methods to print boards

---
+++ BattleShip.Web/Scripts/print-board.js
@@ -0,0 +1,56 @@
+﻿printBoardForPlayer = function () {
+    let columns = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+
+    const tbody = $('<tbody/>');
+
+    let tr = $('<tr/>');
+    for (let c = 0; c < columns.length; c++) {
+        tr.append(`<td>` + columns[c] + `</td>`);
+    }
+    tbody.append(tr);
+
+    for (let r = 1; r <= 10; r++) {
+        let tr = $('<tr/>');
+        tr.append(`<td>` + r + `</td>`);
+
+        for (let c = 1; c < columns.length; c++) {
+            tr.append(`<td class="cell" data-counter="0" data-cell="`
+                    + columns[c]
+                    + `-`
+                    + r
+                    + `"><button class="btn btn-info"></button></td>`);
+        }
+
+        tbody.append(tr);
+    }
+
+    return tbody;
+};
+printBoardForOpponent = function () {
+    let columns = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+
+    const tbody = $('<tbody/>');
+
+    let tr = $('<tr/>');
+    for (let c = 0; c < columns.length; c++) {
+        tr.append(`<td>` + columns[c] + `</td>`);
+    }
+    tbody.append(tr);
+
+    for (let r = 1; r <= 10; r++) {
+        let tr = $('<tr/>');
+        tr.append(`<td>` + r + `</td>`);
+
+        for (let c = 1; c < columns.length; c++) {
+            tr.append(`<td class="cell" data-cell="`
+            + columns[c]
+            + `-`
+            + r
+            + `"><button class="btn btn-info"></button></td>`);
+        }
+
+        tbody.append(tr);
+    }
+
+    return tbody;
+};
