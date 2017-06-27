c1f46a61bd83397fa98b6239b89da1b47589e13a
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-06-28 00:16:23 +0200

    Refactoring

--- BattleShip.Web/Scripts/print-board.js
+++ BattleShip.Web/Scripts/print-board.js
@@ -1,24 +1,27 @@
 ﻿printBoardForPlayer = function () {
+    return printBoard(tdCellsForPlayer);
+};
+printBoardForOpponent = function () {
+    return printBoard(tdCellsForOpponent);
+};
+
+printBoard = function(tdCells) {
     let columns = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
-    const tbody = $('<tbody/>');
+    const tbody = $("<tbody/>");
 
-    let tr = $('<tr/>');
+    let tr = $("<tr/>");
     for (let c = 0; c < columns.length; c++) {
-        tr.append(`<td>` + columns[c] + `</td>`);
+        tr.append(`<td>${columns[c]}</td>`);
     }
     tbody.append(tr);
 
     for (let r = 1; r <= 10; r++) {
-        let tr = $('<tr/>');
-        tr.append(`<td>` + r + `</td>`);
+        let tr = $("<tr/>");
+        tr.append(`<td>${r}</td>`);
 
         for (let c = 1; c < columns.length; c++) {
-            tr.append(`<td class="cell" data-counter="0" data-cell="`
-                    + columns[c]
-                    + `-`
-                    + r
-                    + `"><button class="btn btn-info"></button></td>`);
+            tr.append(tdCells);
         }
 
         tbody.append(tr);
@@ -26,31 +29,11 @@
 
     return tbody;
 };
-printBoardForOpponent = function () {
-    let columns = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
-    const tbody = $('<tbody/>');
+tdCellsForPlayer = function(column, row) {
+    return `<td class="cell" data-counter="0" data-cell="${column}-${row}"><button class="btn btn-info"></button></td>`;
+};
 
-    let tr = $('<tr/>');
-    for (let c = 0; c < columns.length; c++) {
-        tr.append(`<td>` + columns[c] + `</td>`);
-    }
-    tbody.append(tr);
-
-    for (let r = 1; r <= 10; r++) {
-        let tr = $('<tr/>');
-        tr.append(`<td>` + r + `</td>`);
-
-        for (let c = 1; c < columns.length; c++) {
-            tr.append(`<td class="cell" data-cell="`
-            + columns[c]
-            + `-`
-            + r
-            + `"><button class="btn btn-info"></button></td>`);
-        }
-
-        tbody.append(tr);
-    }
-
-    return tbody;
+tdCellsForOpponent = function (column, row) {
+    return `<td class="cell" data-cell="${column}-${row}"><button class="btn btn-info"></button></td>`;
 };
