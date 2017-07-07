f3e9972c5ea3cfe719428f6cacc0201fc3451967
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-07-08 00:11:31 +0200

    Add js BattleViewModel

---
+++ BattleShip.Web/Scripts/ViewModels/battleViewModel.js
@@ -0,0 +1,17 @@
+﻿var BattleViewModel = function() {
+    var self = this;
+
+    self.battles = ko.observableArray();
+
+    var battlesUri = "/api/battles";
+
+    function getAllBattles() {
+        ajaxHelper(battlesUri, "GET").done(function(data) {
+            self.battles(data);
+        });
+    }
+
+    getAllBattles();
+};
+
+ko.applyBindings(new BattleViewModel());
